5c3d7b446585950262130efe114dd3fffbbfc946
zzj <912290893@qq.com>
2021-12-20 00:25:13 +0800

    asdf

---
+++ clion/action3/example/include/shared.h
@@ -0,0 +1,11 @@
+//
+// Created by zzj on 2021/12/19.
+//
+
+#ifndef EXAMPLE3_SHARED_H
+#define EXAMPLE3_SHARED_H
+
+#include <iostream>
+void sayShared();
+
+#endif //EXAMPLE3_SHARED_H
---
+++ clion/action3/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <shared.h>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    sayShared();
+    return 0;
+}
